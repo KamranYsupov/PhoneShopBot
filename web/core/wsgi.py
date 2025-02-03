6466bdcc8dbf3965aa5f01c947dfdbb6678a1e99
Kamran <kamrankek48@gmail.com>
2025-02-03 19:50:16 +0400

    Вернул прежний вид wsgi.py

--- web/core/wsgi.py
+++ web/core/wsgi.py
@@ -1,19 +1,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.db.utils import ProgrammingError
-
-from web.apps.bot_settings.models import BotSettings
     
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.core.settings')
 
 application = get_wsgi_application()
 
-try:
-    bot_settings = BotSettings.get_instance()
-    bot_settings.is_active = True
-    bot_settings.save()
-except ProgrammingError:
-    pass
-    
 
