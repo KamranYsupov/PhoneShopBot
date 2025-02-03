1ccb254ef7d1d2e9b6df17755fb08f26b4de809d
Kamran <kamrankek48@gmail.com>
2025-02-03 19:59:49 +0400

    Изменил отправку сообщений при изменении заказа в админке

--- web/apps/orders/signals.py
+++ web/apps/orders/signals.py
@@ -19,7 +19,19 @@ def order_post_save(sender, instance, created, **kwargs):
             'если готовы приобрести по более высокой цене, '
             'обратитесь к менеджеру'
         )
-        inline_keyboard = [[
+    else:
+        text = '<b>Заказ принят в обработку'
+        text += (
+            '🔥' if instance.status == Order.Status.ARRIVED 
+            else 'с изменениями ❌'
+        ) + '.</b>\n'
+        
+        text += (
+            'С накладкой на сегодняшний день '
+            'вы можете ознакомиться в разделе "Мои заказы".'
+        )  
+           
+    inline_keyboard = [[
         {
             'text': 'Открыть заказ',
             'callback_data': f'order_{instance.id}'
@@ -27,12 +39,12 @@ def order_post_save(sender, instance, created, **kwargs):
     ]]
          
     
-        telegram_service.send_message(
+    telegram_service.send_message(
         chat_id=instance.buyer.telegram_id,
         text=text,
         reply_markup={'inline_keyboard': inline_keyboard}
     )
-            
+    
 
 @receiver(post_delete, sender=Order)
 def order_post_delete(sender, instance, **kwargs):
